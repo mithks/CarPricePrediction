c90db7504954bf0205fccbabbcdc488f6a18b7d0
K S MITHUN <72300231+mithks@users.noreply.github.com>
2024-05-26 15:50:56 +0530

    Uploaded model, dataset, web application

---
+++ app.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as numpy
+import pickle as pk
+import streamlit as st
+
+model = pk.load(open('model.pkl', 'rb'))
+
+st.header('Predict Car Price using ML')
+
+cars_data = pd.read_csv(r'C:/Users/mithu/Documents/MLproj/Cardetails.csv')
+
+def get_brand_name(car_name):
+    car_name = car_name.split(' ')[0]
+    return car_name.strip()
+
+cars_data['name'] = cars_data['name'].apply(get_brand_name)
+
+name = st.selectbox('Car Brand', cars_data['name'].unique())
+year = st.slider('Year of Manufacture', 1994, 2024)
+km_driven = st.slider('No of kms driven', 0, 200000)
+fuel = st.selectbox('Fuel Type', cars_data['fuel'].unique())
+seller_type = st.selectbox('Seller', cars_data['seller_type'].unique())
+transmission = st.selectbox('Transmission', cars_data['transmission'].unique())
+owner = st.selectbox('Owner', cars_data['owner'].unique())
+mileage = st.slider('Mileage', 5, 40)
+engine = st.slider('Engine CC', 700, 7000)
+max_power = st.slider('Max Power', 0, 700)
+seats = st.slider('Seats', 5, 10)
+
+if st.button("Predict"):
+    input_data_model = pd.DataFrame([[name,year,km_driven,fuel,seller_type,transmission,owner,mileage,engine,max_power,seats]], 
+                                    columns = ['name','year','km_driven','fuel','seller_type','transmission','owner','mileage','engine','max_power','seats'])
+    
+    input_data_model['owner'].replace(['First Owner', 'Second Owner', 'Third Owner', 'Fourth & Above Owner', 'Test Drive Car'], [1,2,3,4,5], inplace = True)
+    input_data_model['fuel'].replace(['Diesel', 'Petrol', 'LPG', 'CNG'], [1,2,3,4], inplace = True)
+    input_data_model['seller_type'].replace(['Individual', 'Dealer', 'Trustmark Dealer'], [1,2,3], inplace = True)
+    input_data_model['transmission'].replace(['Manual', 'Automatic'], [1,2], inplace = True)
+    input_data_model['name'].replace(['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
+       'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz',
+       'Mitsubishi', 'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus',
+       'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force',
+       'Ambassador', 'Ashok', 'Isuzu', 'Opel'], [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31], inplace = True)
+
+    car_price = model.predict(input_data_model)
+
+    st.markdown('Car Price is Rs. ' + str(car_price[0]))
